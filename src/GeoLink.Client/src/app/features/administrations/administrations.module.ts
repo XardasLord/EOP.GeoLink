ae258c21ded5b5ef1086ab124be78af6558f5e18
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2022-12-14 09:32:12 +0100

    Add groups-management and roles-management components

--- src/GeoLink.Client/src/app/features/administrations/administrations.module.ts
+++ src/GeoLink.Client/src/app/features/administrations/administrations.module.ts
@@ -3,20 +3,25 @@ import { NgxsModule } from '@ngxs/store';
 import { SharedModule } from '../../shared/shared.module';
 import { AdministrationComponent } from './components/administration/administration.component';
 import { AdministrationRoutingModule } from './administrations-routing.module';
-import { UserManagementComponent } from './components/user-management/user-management.component';
 import { AdministrationsState } from './state/administrations.state';
-import { IAdministrationsService } from './services/administrations.service.base';
-import { AdministrationsService } from './services/administrations.service';
+import { UsersService } from './services/users.service';
+import { UsersManagementComponent } from './components/users-management/users-management.component';
+import { GroupsManagementComponent } from './components/groups-management/groups-management.component';
+import { RolesManagementComponent } from './components/roles-management/roles-management.component';
+import { IUsersService } from './services/users.service.base';
 
 @NgModule({
-  declarations: [AdministrationComponent, UserManagementComponent],
+  declarations: [
+    AdministrationComponent,
+    UsersManagementComponent,
+    GroupsManagementComponent,
+    RolesManagementComponent,
+  ],
   imports: [
     SharedModule,
     AdministrationRoutingModule,
     NgxsModule.forFeature([AdministrationsState]),
   ],
-  providers: [
-    { provide: IAdministrationsService, useClass: AdministrationsService },
-  ],
+  providers: [{ provide: IUsersService, useClass: UsersService }],
 })
 export class AdministrationsModule {}
---
+++ src/GeoLink.Client/src/app/features/administrations/components/groups-management/groups-management.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-groups-management',
+  templateUrl: './groups-management.component.html',
+  styleUrls: ['./groups-management.component.scss']
+})
+export class GroupsManagementComponent {
+
+}
