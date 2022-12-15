0d6448e57334c821674f5941f533caa2783f7466
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2022-12-15 16:05:41 +0100

    Enable Debug Tools for performance debugging purposes

--- src/GeoLink.Client/src/main.ts
+++ src/GeoLink.Client/src/main.ts
@@ -1,7 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableDebugTools } from '@angular/platform-browser';
+import { ApplicationRef } from '@angular/core';
 
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
+  .then(module =>
+    enableDebugTools(module.injector.get(ApplicationRef).components[0])
+  )
   .catch(err => console.error(err));
