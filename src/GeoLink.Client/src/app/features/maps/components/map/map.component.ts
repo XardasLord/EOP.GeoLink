835ad44a503fe1b16c02c14677985acdb9b9c1b6
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2022-12-16 09:55:12 +0100

    Install Marker Cluster plugin for leaflet to allow grouping the markers on the map. Add sample demo map with marker cluster in use

--- src/GeoLink.Client/src/app/features/maps/components/map/map.component.ts
+++ src/GeoLink.Client/src/app/features/maps/components/map/map.component.ts
@@ -1,21 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LeafletControlLayersConfig } from '@asymmetrik/ngx-leaflet';
-import {
-  circle,
-  latLng,
-  Layer,
-  MapOptions,
-  marker,
-  polygon,
-  tileLayer,
-} from 'leaflet';
+import { circle, latLng, Layer, MapOptions, marker, polygon, tileLayer } from 'leaflet';
+import * as L from 'leaflet';
+import 'leaflet.markercluster';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss'],
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
   // https://leafletjs.com/examples/wms/wms.html
   options: MapOptions = {
     layers: [
@@ -25,8 +19,7 @@ export class MapComponent {
         minZoom: 3,
         maxZoom: 19,
         detectRetina: true,
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }),
     ],
     zoom: 7,
@@ -45,22 +38,16 @@ export class MapComponent {
         maxZoom: 18,
         attribution: '...',
       }),
-      'Topography, then places': tileLayer.wms(
-        'http://ows.mundialis.de/services/service?',
-        {
-          layers: 'TOPO-WMS,OSM-Overlay-WMS',
-          maxZoom: 18,
-          attribution: '...',
-        }
-      ),
-      'Places, then topography': tileLayer.wms(
-        'http://ows.mundialis.de/services/service?',
-        {
-          layers: 'OSM-Overlay-WMS,TOPO-WMS',
-          maxZoom: 18,
-          attribution: '...',
-        }
-      ),
+      'Topography, then places': tileLayer.wms('http://ows.mundialis.de/services/service?', {
+        layers: 'TOPO-WMS,OSM-Overlay-WMS',
+        maxZoom: 18,
+        attribution: '...',
+      }),
+      'Places, then topography': tileLayer.wms('http://ows.mundialis.de/services/service?', {
+        layers: 'OSM-Overlay-WMS,TOPO-WMS',
+        maxZoom: 18,
+        attribution: '...',
+      }),
     },
     overlays: {
       'Big Circle': circle([46.95, -122], { radius: 5000 }),
@@ -80,10 +67,61 @@ export class MapComponent {
       [46.92, -121.92],
       [46.87, -121.8],
     ]),
-    marker([52.22779941887071, 19.764404296875]),
+    marker([52.22779941887071, 19.764404296875], {
+      icon: L.icon({
+        iconSize: [25, 41],
+        iconAnchor: [13, 41],
+        iconUrl: 'assets/leaflet/marker-icon.png',
+        iconRetinaUrl: 'assets/leaflet/marker-icon-2x.png',
+        shadowUrl: 'assets/leaflet/marker-shadow.png',
+      }),
+    }),
   ];
 
   mapClicked(event: any) {
     console.log(event);
+  }
+
+  // Marker cluster configuration
+  markerClusterGroup!: L.MarkerClusterGroup;
+  markerClusterData: L.Marker[] = [];
+  markerClusterOptions!: L.MarkerClusterGroupOptions;
+
+  // Generators for lat/lon values
+  generateLat() {
+    return Math.random() * 360 - 180;
+  }
+  generateLon() {
+    return Math.random() * 180 - 90;
+  }
+
+  ngOnInit() {
+    this.refreshData();
+  }
+
+  markerClusterReady(group: L.MarkerClusterGroup) {
+    this.markerClusterGroup = group;
+  }
+
+  refreshData(): void {
+    this.markerClusterData = this.generateData(10000);
+  }
+
+  generateData(count: number): L.Marker[] {
+    const data: L.Marker[] = [];
+
+    for (let i = 0; i < count; i++) {
+      const icon = L.icon({
+        iconSize: [25, 41],
+        iconAnchor: [13, 41],
+        iconUrl: 'assets/leaflet/marker-icon.png',
+        iconRetinaUrl: 'assets/leaflet/marker-icon-2x.png',
+        shadowUrl: 'assets/leaflet/marker-shadow.png',
+      });
+
+      data.push(L.marker([this.generateLat(), this.generateLon()], { icon }));
+    }
+
+    return data;
   }
 }
--- src/GeoLink.Client/src/app/features/maps/maps.module.ts
+++ src/GeoLink.Client/src/app/features/maps/maps.module.ts
@@ -4,9 +4,10 @@ import { SharedModule } from '../../shared/shared.module';
 import { MapRoutingModule } from './maps-routing.module';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { MapHelperBarComponent } from './components/map-helper-bar/map-helper-bar.component';
+import { LeafletMarkerClusterModule } from '@asymmetrik/ngx-leaflet-markercluster';
 
 @NgModule({
   declarations: [MapComponent, MapHelperBarComponent],
-  imports: [SharedModule, MapRoutingModule, LeafletModule],
+  imports: [SharedModule, MapRoutingModule, LeafletModule, LeafletMarkerClusterModule],
 })
 export class MapsModule {}
