3014363aa957a8c217c6991572d38841cb761e60
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2022-12-28 11:00:17 +0100

    Map object filters initialization

--- src/GeoLink.Client/src/app/features/maps/components/map-helper-bar/map-helper-bar.component.ts
+++ src/GeoLink.Client/src/app/features/maps/components/map-helper-bar/map-helper-bar.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './map-helper-bar.component.html',
   styleUrls: ['./map-helper-bar.component.scss'],
 })
-export class MapHelperBarComponent {}
+export class MapHelperBarComponent {
+  showObjectFilters = false;
+
+  toogleObjectFilters(): void {
+    this.showObjectFilters = !this.showObjectFilters;
+  }
+}
