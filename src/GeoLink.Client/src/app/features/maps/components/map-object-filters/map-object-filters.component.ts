d097ce4f8558f45e2c541d694e4398cdfe37f867
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2022-12-29 21:01:22 +0100

    Add a separate new component that represents map's object filters

---
+++ src/GeoLink.Client/src/app/features/maps/components/map-object-filters/map-object-filters.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-object-filters',
+  templateUrl: './map-object-filters.component.html',
+  styleUrls: ['./map-object-filters.component.scss']
+})
+export class MapObjectFiltersComponent {
+
+}
--- src/GeoLink.Client/src/app/features/maps/maps.module.ts
+++ src/GeoLink.Client/src/app/features/maps/maps.module.ts
@@ -6,9 +6,10 @@ import { SharedModule } from '../../shared/shared.module';
 import { MapRoutingModule } from './maps-routing.module';
 import { MapHelperBarComponent } from './components/map-helper-bar/map-helper-bar.component';
 import { MapItemTooltipDialogComponent } from './components/map-item-tooltip-dialog/map-item-tooltip-dialog.component';
+import { MapObjectFiltersComponent } from './components/map-object-filters/map-object-filters.component';
 
 @NgModule({
-  declarations: [MapComponent, MapHelperBarComponent, MapItemTooltipDialogComponent],
+  declarations: [MapComponent, MapHelperBarComponent, MapItemTooltipDialogComponent, MapObjectFiltersComponent],
   imports: [SharedModule, MapRoutingModule, LeafletModule, LeafletMarkerClusterModule],
 })
 export class MapsModule {}
