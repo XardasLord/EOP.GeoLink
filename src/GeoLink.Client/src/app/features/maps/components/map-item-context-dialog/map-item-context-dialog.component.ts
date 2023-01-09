d3af27f23498e207e65d58da85e7171ba6f0cbfc
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2023-01-09 15:19:12 +0100

    Temporary solution for showing device submenu (second level)

--- src/GeoLink.Client/src/app/features/maps/components/map-item-context-dialog/map-item-context-dialog.component.ts
+++ src/GeoLink.Client/src/app/features/maps/components/map-item-context-dialog/map-item-context-dialog.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentChecked, Component } from '@angular/core';
+import { AfterContentChecked, ChangeDetectorRef, Component } from '@angular/core';
 import { DeviceItemModel, MapItemModel } from '../../models/map-item.model';
 
 @Component({
@@ -8,8 +8,11 @@ import { DeviceItemModel, MapItemModel } from '../../models/map-item.model';
 })
 export class MapItemContextDialogComponent implements AfterContentChecked {
   public mapItem!: MapItemModel;
+  public showSubMenu = false;
+  public topCssValue = '';
 
-  constructor() {
+  constructor(private changeDetectorRef: ChangeDetectorRef) {
+    // https://indepth.dev/posts/1054/here-is-what-you-need-to-know-about-dynamic-components-in-angular#ngonchanges
     console.warn('call from constructor');
   }
 
@@ -19,5 +22,30 @@ export class MapItemContextDialogComponent implements AfterContentChecked {
 
   showStatusChart(deviceItem: DeviceItemModel) {
     console.log('Showing device item chart...', deviceItem);
+  }
+
+  showDeviceSubMenu(deviceItem: DeviceItemModel, e: MouseEvent) {
+    console.log('Showing device item submenu...', deviceItem);
+    this.showSubMenu = true;
+
+    const popupHeight = 400,
+      popupWidth = 250;
+
+    let xPosition, yPosition;
+    if (e.clientX + popupWidth > window.innerWidth) {
+      xPosition = e.pageX - popupWidth;
+    } else {
+      xPosition = e.pageX;
+    }
+
+    if (e.clientY + popupHeight > window.innerHeight) {
+      yPosition = e.pageY - popupHeight;
+    } else {
+      yPosition = e.pageY;
+    }
+
+    this.topCssValue = yPosition + 'px';
+
+    this.changeDetectorRef.detectChanges();
   }
 }
