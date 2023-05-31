7ccfea5dd29870beeea35ff214e42b844fdf0dce
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2023-05-31 22:16:56 +0200

    Extend JWT Token representation model with new information about initial filters

--- src/GeoLink.Client/src/app/shared/auth/models/user-auth.model.ts
+++ src/GeoLink.Client/src/app/shared/auth/models/user-auth.model.ts
@@ -7,6 +7,12 @@ export class UserAuthModel {
   email: string | undefined;
   role: string | undefined;
   auth_scopes: AuthScopes[] | undefined;
+  regions: number[] | undefined;
+  groups: number | undefined;
+  init_objecttypefilters: number | undefined;
+  init_devicefilters: number[] | undefined;
+  init_regionfilters: number[] | undefined;
+  init_statusfilters: number[] | undefined;
 
   constructor(init?: Partial<UserAuthModel>) {
     Object.assign(this, { ...init });
