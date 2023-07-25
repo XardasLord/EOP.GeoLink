ac712060d2cc46fb2809a62a681ab49ddd9e8bfd
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2023-07-25 07:22:21 +0200

    Get rid of unused property from charts response model

--- src/GeoLink.Client/src/app/shared/models/charts/device-chart.model.ts
+++ src/GeoLink.Client/src/app/shared/models/charts/device-chart.model.ts
@@ -10,7 +10,6 @@ export interface ChartGeneralData {
   chartName: string;
   avgAvail: number; // Średnia dostępność (0,100)
   devHealth: number; // Kondycja urządzenia (1,2,3)
-  trendLineCoeffs: number[]; // Współczynniki wielomianu do narysowania linii trendu, dla avail - wielomian 2 stopnia (3 współczynniki), dla reszty wielomian 1 stopnia (2 współczynniki - linia)
   data: ChartData[]; // array z szeregiem czasowym do wyrzucenia na wykres
 }
 
