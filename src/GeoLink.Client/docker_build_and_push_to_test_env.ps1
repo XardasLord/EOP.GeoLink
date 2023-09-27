19b4a91ccc71be76c717b8b169e01f39501b7025
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2023-09-27 11:35:59 +0200

    Automatize docker build and push powershell script to tag image automatically based on the version.ts APP_VERSION value

--- src/GeoLink.Client/docker_build_and_push_to_test_env.ps1
+++ src/GeoLink.Client/docker_build_and_push_to_test_env.ps1
@@ -1,6 +1,8 @@
 # Builds and pushes docker image to test environment
-docker build -f Dockerfile.test -t geolink-client:test .
-docker tag geolink-client:test 10.0.2.71:5000/geolink-client:test
-docker push 10.0.2.71:5000/geolink-client:test
+$version = Get-Content -Path .\src\app\version.ts | Select-String -Pattern "export const APP_VERSION" | ForEach-Object { $_ -replace "export const APP_VERSION = '|';", '' }
+
+docker build -f Dockerfile.test -t geolink-client:$version .
+docker tag geolink-client:$version 10.0.2.71:5000/geolink-client:$version
+docker push 10.0.2.71:5000/geolink-client:$version
 
 # ssh pkowalewicz@10.0.2.71
