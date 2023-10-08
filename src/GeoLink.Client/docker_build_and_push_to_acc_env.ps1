56d5e2ca059f21da2b7d04734b03e40a996b1213
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2023-10-08 20:39:02 +0200

    Change deploy to EOP ACC server by using application version tags

--- src/GeoLink.Client/docker_build_and_push_to_acc_env.ps1
+++ src/GeoLink.Client/docker_build_and_push_to_acc_env.ps1
@@ -1,6 +1,8 @@
 # Builds and pushes docker image to test environment
-docker build -f Dockerfile.acc -t geolink-client:acc .
-docker tag geolink-client:acc host.docker.internal:3000/geolink-client:acc
-docker push host.docker.internal:3000/geolink-client:acc
+$version = Get-Content -Path .\src\app\version.ts | Select-String -Pattern "export const APP_VERSION" | ForEach-Object { $_ -replace "export const APP_VERSION = '|';", '' }
+
+docker build -f Dockerfile.acc -t geolink-client:$version .
+docker tag geolink-client:$version host.docker.internal:3000/geolink-client:$version
+docker push host.docker.internal:3000/geolink-client:$version
 
 # ssh energa\01Z00644@fudo2.eite.energa.loc:266
