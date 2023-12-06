60d3cd9f3cba3045176bc91bb47a900123917414
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2023-12-06 09:20:40 +0100

    Remove pushing docker image on production since the deployment is done using a TAR package uploaded manually

--- src/GeoLink.Client/docker_build_and_push_to_prod_env.ps1
+++ src/GeoLink.Client/docker_build_and_push_to_prod_env.ps1
@@ -2,6 +2,3 @@
 $version = Get-Content -Path .\src\app\version.ts | Select-String -Pattern "export const APP_VERSION" | ForEach-Object { $_ -replace "export const APP_VERSION = '|';", '' }
 
 docker build -f Dockerfile.prod -t geolink-client:$version .
-docker tag geolink-client:$version host.docker.internal:3000/geolink-client:$version
-docker push host.docker.internal:3000/geolink-client:$version
-
