6291d156bd7e82c6108ed1fd7c0387e6bff0a786
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2023-12-06 09:11:29 +0100

    Add and adjust production dockerfile and builder script

--- src/GeoLink.Client/docker_build_and_push_to_prod_env.ps1
+++ src/GeoLink.Client/docker_build_and_push_to_prod_env.ps1
@@ -1,8 +1,7 @@
-# Builds and pushes docker image to ACC environment
+# Builds and pushes docker image to production environment
 $version = Get-Content -Path .\src\app\version.ts | Select-String -Pattern "export const APP_VERSION" | ForEach-Object { $_ -replace "export const APP_VERSION = '|';", '' }
 
-docker build -f Dockerfile.acc -t geolink-client:$version .
+docker build -f Dockerfile.prod -t geolink-client:$version .
 docker tag geolink-client:$version host.docker.internal:3000/geolink-client:$version
 docker push host.docker.internal:3000/geolink-client:$version
 
-# ssh energa\01Z00644@fudo2.eite.energa.loc:266
